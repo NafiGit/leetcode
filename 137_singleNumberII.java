f5b39fce61a3a7c695d0bfe68390adf589aac038
Nahfid Nissar <105824411+NafiGit@users.noreply.github.com>
2023-09-21 23:08:18 +0530

    Create 137_singleNumberII.java

---
+++ 137_singleNumberII.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+
+        Map<Integer, Integer> fm = new HashMap<>();
+        for(int i=0;i<nums.length; i++){
+            if(fm.containsKey(nums[i])){
+                fm.put(nums[i], fm.get(nums[i])+1);
+            } else{
+                fm.put(nums[i], 1);
+            }
+            System.out.println(nums[i]);
+
+        }
+        System.out.println(fm.toString());
+
+        for(Map.Entry<Integer, Integer> entry: fm.entrySet()){
+            if(entry.getValue() == 1){
+                return entry.getKey();
+            }
+        }
+        return nums[0];
+    }
+}
